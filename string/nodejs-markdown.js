39dd93fe177cec388584bcc99eb5abe1dd8cadd8
Bangyi Jiang <overblued@gmail.com>
2015-01-24 02:04:29 +0800

    change how ordered list get parsed

--- string/nodejs-markdown.js
+++ string/nodejs-markdown.js
@@ -70,17 +70,13 @@ var syntax = [
 			return '<blockquote>\n' + m.replace(/^>/gm, '') + '</blockquote>\n'
 		}
 	},
-	//list
-	{	regex:/^1\./gm,
-		sub: '<ol><li>\n'
-	},
+	// ordered list
 	{
-		regex: /^(\d{1,2}\.)/gm,
-		sub: "</li><li>\n"
-	},
-
-	{	regex:/\?listend$/gm,
-		sub: '</li></ol>'
+		regex:/^\s*1\..*\n(?:[\d\s].*\n)+/gm,
+		sub: function (m) {
+			return '<ol><li>\n' + m.replace(/^\s*1\./,'').replace(/^\s*(\d{1,2}\.)/gm, '</li><li>\n') + '</li></ol>'
+		}
+	
 	},
 	//code block
 	{	regex: /^```(\w+)\n/gm,
@@ -137,18 +133,7 @@ function md(text){
 				.replace(/\r\n/g,'\n')
 				.replace(/\n+/g,'\n')
 		;//		.split('\n');
-	//bootstap the end of some multiline block
-	var stack = text.split('\n');
-	findIndexs(stack,function (t) {
-		return /^1\./gm.test(t);
-	}).forEach(function(i){
-		var l = findIndex(stack,[i],function(t){
-			return /^[^\s\d]/.test(t);
-		})
-		stack[l-1] = stack[l-1] + '?listend'
-	})
-	
-	text = stack.join("\n");
+
 	
 	syntax.forEach(function (syn) {
 		text = text.replace(syn.regex, syn.sub)
@@ -158,27 +143,7 @@ function md(text){
 	return preHTML + text;
 }
 
-//utilities
-//find the index of certain element in an array
-function findIndexs(a, fn) {
-	var i = 0,len = a.length,result = [];
-	for (i; i < len; i++) {
-		if (fn(a[i]))
-			result.push(i);
-	}
-	return result;
-}
-function findIndex(a, range,fn) {
-	if (typeof range == 'function'){
-		fn = range;
-		range = [0];
-	}
-	var i = range[0],len = range[1] || a.length;
-	for (i; i < len; i++) {
-		if (fn(a[i]))
-			return i;
-	}
-}
+
 if (typeof exports === 'object') {
   module.exports = md;
 } 
